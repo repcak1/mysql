9158af53e458f9bf3c074874798396100b9e0d3a
repcak1 <113353486+repcak1@users.noreply.github.com>
2023-10-20 12:14:04 +0200

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -1,1 +1,15 @@
 <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+
+//create connection 
+$conn = mysqli_connect($servername,$username,$password);
+
+//check information
+ if (!$conn) {
+    die("connection failed:" . mysqli_connect_error() );
+ }
+ echo "connected successfully";
+?>
