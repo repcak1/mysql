fd7cf0812981fa474ff4fae2c3e7159a0d5e4f05
repcak1 <113353486+repcak1@users.noreply.github.com>
2023-10-27 07:57:32 +0200

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -61,5 +61,16 @@ if (mysqli_query($conn, $sql)) {
 } else {
     echo "error: ". $sql. "<br>". mysqli_error($conn);
  }
- 
+ $sql = "SELECT  id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) > 0) {
+
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id:" . $row["id"]. "- Name:" . 
+        $row["firstname"]. "" . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 result";
+}
   ?>
